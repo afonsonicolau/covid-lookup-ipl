92ef66837d1ac0943293b9fef452ec8ad72edc71
nicolau <afonso9980@gmail.com>
2021-12-22 19:31:27 +0000

    Initial structures development

--- main.c
+++ main.c
@@ -4,8 +4,46 @@
 #include <ctype.h>
 #include <locale.h>
 
-int main()
-{
-    
+// Constants
+#define MAX_MEMBERS 200
+
+// Structures
+typedef struct {
+    int day;
+    int month;
+    int year;
+} date;
+
+typedef struct {
+    int hour;
+    int minute;
+} time;
+
+typedef struct {
+    int snsNumber;
+    char name[50];
+    date birthDate;
+    int memberRole;
+    int confinmentState;
+    int vaccinationState;
+    date lastVaccine;
+} communityMember;
+
+typedef struct {
+    int id;
+    int snsNumber;
+    int type;
+    int result;
+    int duration;
+    time timeTaken;
+    date dateTaken;
+} diagnosticTest;
+
+// Functions
+
+// Main function
+int main() {
+    communityMember member[MAX_MEMBERS];
+
     return 0;
 }
